636df297cb8ca9c65466d00579c6bef57b6e592f
yogniku <156093318+yogniku@users.noreply.github.com>
2024-02-20 12:57:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getFormvalue() {
   let inps=document.querySelectorAll("input");
 	let first_name=inps[0].value;
 let last_name=inps[1].value;
- let fullName=first_name+""+last_name;
+ let fullName=first_name+" "+last_name;
     alert(fullName);
 }
 
