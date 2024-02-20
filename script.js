d7ebd4aa46f69f713c86602125e6a08f181c2684
yogniku <156093318+yogniku@users.noreply.github.com>
2024-02-20 12:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function getFormvalue() {
-    //Write your code here
-	let inps=document.querySelectorAll(input);
-	let first_name=inps[0].value
-	let last_name=inps[1].value
-	let fullName=first_name+" "+last_name
-	
-alert(fullName)
+  let inps=document.querySelectorAll("input");
+	let first_name=inps[0].value;
+let last_name=inps[1].value;
+ let fullName=first_name+""+last_name;
+    alert(fullName);
 }
+
+
+
 
